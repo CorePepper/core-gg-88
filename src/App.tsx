39c8c0070510129d40aeb1da9cbbcb6f534fc4b7
CorePepper <becorep884@gmail.com>
2024-12-25 18:47:48 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,9 @@
 // src/App.tsx
 
-import { Toaster as Sonner } from "@components/ui/toaster";
-import { TooltipProvider } from "@components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// ▼ 以下2つのコンポーネントを使う前提。
-//    src/pages/Index.tsx と src/pages/Reviews.tsx に置いておきます。
-import Index from "./pages/Index";
-import Reviews from "./pages/Reviews";
-
-// React Query 用のクライアントを作成
-const queryClient = new QueryClient();
-
-const App = () => {
+export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        {/* トースト通知用コンポーネント（Sonner） */}
-        <Sonner />
-
-        <BrowserRouter>
-          <Routes>
-            {/* 
-              "/" にアクセスしたら Index を表示
-              "/reviews" にアクセスしたら Reviews を表示
-            */}
-            <Route path="/" element={<Index />} />
-            <Route path="/reviews" element={<Reviews />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </QueryClientProvider>
+    <h1 style={{ color: "white", textAlign: "center", marginTop: "2rem" }}>
+      テスト表示
+    </h1>
   );
-};
-
-export default App;
+}
