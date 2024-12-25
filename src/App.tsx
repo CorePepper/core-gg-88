8d97a4ba5d7172c6a2de72c94af8924e79b662f2
CorePepper <becorep884@gmail.com>
2024-12-25 19:18:06 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 // src/App.tsx
 
 export default function App() {
-  return <h1 style={{color: "red"}}>Hello Test</h1>;
+  return (
+    <h1 style={{ color: "red", textAlign: "center", marginTop: "2rem" }}>
+      Hello Test
+    </h1>
+  );
 }
