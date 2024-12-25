d18b53ed2c8d9ce82c1330cc2d6a7fb9d14fffab
CorePepper <becorep884@gmail.com>
2024-12-26 02:23:16 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
+// src/App.tsx
+
+import { Toaster as Sonner } from "@components/ui/toaster";
+import { TooltipProvider } from "@components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// ▼ 以下の2つを「src/pages/」に置いている前提。
+//    もしフォルダ名が違う場合は importパスを調整してください。
 import Index from "./pages/Index";
 import Reviews from "./pages/Reviews";
 
+// React Query 用のクライアントを生成
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/reviews" element={<Reviews />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        {/* トースト通知用コンポーネント (Sonner) */}
+        <Sonner />
+
+        <BrowserRouter>
+          <Routes>
+            {/* 
+              path="/" でアクセスしたときは <Index /> を表示 
+              path="/reviews" なら <Reviews /> を表示
+            */}
+            <Route path="/" element={<Index />} />
+            <Route path="/reviews" element={<Reviews />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
